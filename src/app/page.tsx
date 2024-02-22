45d16622c306d25f3d82317598615171d42f9d34
yzm <13884935@qq.com>
2024-02-22 21:47:39 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         
         <h1>Hi YZM yes</h1>
-        
       </div>
     </main>
   );
